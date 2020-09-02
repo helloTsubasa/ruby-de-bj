92f188b4dbe1e61cb07a9d65f4f5e628a06b3a23
helloTsubasa <tbs.kishi590@icloud.com>
2020-09-02 23:30:57 +0900

    Update card.rb

--- card.rb
+++ card.rb
@@ -9,12 +9,3 @@ class Card
     return "#{@suit}  of #{@number}"
   end
 
-# J,Q,Kの処理
-  def count
-    if @number == "J" || @number == "Q" || @number == "K"
-      return @number = 10
-    else
-      return @number.to_i
-    end
-  end
-end
